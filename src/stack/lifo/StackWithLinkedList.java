86c70dd339988d911d14bd45996b3e03b756bbb3
Benfarhat <benfarhat.elyes@gmail.com>
2019-03-31 01:13:39 +0100

    Bidirectional linked node (getprev and getnext avail.) for stack implementation

--- src/stack/lifo/StackWithLinkedList.java
+++ src/stack/lifo/StackWithLinkedList.java
@@ -1,9 +1,39 @@
 package stack.lifo;
 
+import java.util.EmptyStackException;
+
 public class StackWithLinkedList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ImplStackWithLinkedList stack = new ImplStackWithLinkedList();
+		for (int i = 1; i <= 10; i++)
+			stack.push(i);
+		stack.print();
+		
+		// 2 pops
+		stack.pop();
+		stack.pop();
+		stack.print();
+		
+		for (int i = 11; i <= 15; i++)
+			stack.push(i);	
+		
+        stack.print();
+        
+        stack.pop();
+        
+        stack.push(16);
+        stack.push(17);
+
+        stack.print();
+  
+        System.out.println("Size of stack is: " + stack.size());
+
+        stack.pop();
+        stack.pop();
+        
+        stack.print();
+        System.out.println("Top element of stack is: " + stack.top());
 
 	}
 
@@ -11,11 +41,11 @@ public class StackWithLinkedList {
 
 class ImplStackWithLinkedList {
 	private int length;
-	private ListNode topOfStack;
+	private ListNode top;
 	
 	public ImplStackWithLinkedList() {
 		length = 0;
-		topOfStack = null;
+		top = null;
 	}
 	
 	public boolean isEmpty() {
@@ -28,37 +58,87 @@ class ImplStackWithLinkedList {
 	
 	public void push(int data) {
 		ListNode item = new ListNode(data);
-		item.setNext(topOfStack);
-		topOfStack = item;
+		if (!isEmpty()) {
+			top.setNext(item);
+			item.setPrev(top);
+		} 
+		
+		top = item;
 		length++;
 	}
-	
+
 	public int pop() {
-		if (isEmpty)
-			throw new EmptyStackException;
+		if (isEmpty())
+			throw new EmptyStackException();
+		int result = top.getData();
+		top = top.getPrev();
+		top.setNext(null);
+		length--;
+		return result;
+	}
+	
+	public int top() {
+		if (isEmpty())
+			throw new EmptyStackException();
+
+		return top.getData();
+	}
+	
+	public String toString() {
+		StringBuilder result =  new StringBuilder();
+		ListNode current = top;
+		while (current != null) {
+			result.append(current.toString() + "\n");
+
+			current = current.getPrev();
+		}
+		return result.toString();
+	}
+	
+	public void print() {
+		System.out.println("\n" + toString() + "\n");
 	}
 }
 
 class ListNode {
 	public ListNode next;
+	public ListNode prev;
 	public int data;
 
 	public ListNode() {
 		next = null;
+		prev = null;
 		data = Integer.MIN_VALUE;
 	}
 	
 	public ListNode(int data) {
 		next = null;
+		prev = null;
 		this.data = data;
 	}
 	
 	public ListNode getNext() {
 		return next;
-	}
+	}	
 	
+	public ListNode getPrev() {
+		return prev;
+	}
+
 	public void setNext(ListNode node) {
 		next = node;
+	}
+	
+	public void setPrev(ListNode node) {
+		prev = node;
+	}
+
+	public boolean hasNext() {
+		return (next != null);
+	}
+	
+	public boolean hasPrev() {
+		return (prev != null);
 	}
 	
 	public int getData() {
