5d11b6dd2d599bc74d553cc86469ecc1bef34656
Benfarhat <benfarhat.elyes@gmail.com>
2019-04-02 03:33:05 +0100

    Initial Generic List Node

---
+++ src/linked/list/GenericListNode.java
@@ -0,0 +1,28 @@
+package linked.list;
+
+public class GenericListNode<T> {
+	public GenericListNode<T> next;
+	public T data;
+	public GenericListNode(T data) {
+		next = null;
+		this.data = data;
+	}
+	public GenericListNode() {
+		this(null);
+	}
+	public GenericListNode<T> getNext() {
+		return next;
+	}
+	public void setNext(GenericListNode<T> next) {
+		this.next = next;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public String toString() {
+		return String.valueOf(data);
+	}
+}
